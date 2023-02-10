b479d90f57bc8e2250e24eef6c0c4195a72cc273
Rafael Moreira Ramos de Rezende <100146657+UPraggy@users.noreply.github.com>
2023-02-10 14:53:40 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import {View,Text} from 'react-native'
+import MyApps from './components/MyApps'
+import MeuEstilo from './components/statics/MeuEstilo'
+
+export default function My_App(){
+  return ( 
+      <MyApps />
+    );
+};
